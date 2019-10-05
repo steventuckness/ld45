e8a6872259d9717c143b8694485420f2a35e59f4
steven tuckness <stuckne1@gmail.com>
2019-10-05 18:59:24 -0500

    add HUD

---
+++ Assets/Hud.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Hud : MonoBehaviour
+{
+    public int gravityWells = 4; // TODO: set from somewhere else
+
+    public Text gravityWellsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gravityWellsText.text = "Gravity Well(s): " + gravityWells.ToString();
+    }
+}
