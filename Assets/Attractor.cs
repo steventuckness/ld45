c605fc8a6bfd1b6217ab15b2c3a4c5949ced7187
Steven Barnhurst <stevenbarnhurst@gmail.com>
2019-10-05 17:41:22 -0500

    add gravity... bitches

---
+++ Assets/Attractor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attractor : MonoBehaviour
+{
+
+    const float G = 6.674f;
+    
+    public Rigidbody rb;
+
+    public static List<Attractor> Attractors;
+
+    void FixedUpdate() {
+        foreach(Attractor attractor in Attractors)
+        {
+            if (attractor != this)
+                Attract(attractor);
+        }
+    }
+
+    private void OnEnable()
+    {
+        if (Attractors == null)
+            Attractors = new List<Attractor>();
+
+        Attractors.Add(this);
+    }
+
+    void Attract (Attractor objToAttract)
+    {
+        Rigidbody rbToAttract = objToAttract.rb;
+
+        Vector3 direction = rb.position - rbToAttract.position;
+        float distance = direction.magnitude;
+
+        if (distance == 0f)
+            return; 
+
+        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAttract.AddForce(force);
+
+    }
+}
