0d10111adb04ba075a975b8bc7257978eeda315c
Leonardo Sa <leo@leo-sa.com>
2019-10-05 19:23:13 -0500

    Gravity wells

---
+++ Assets/Scripts/GravityWell.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWell : MonoBehaviour
+{
+    public float Duration;
+    private float timeRemaining;
+    private float timeDelta;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeRemaining = Duration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/GravityWellSpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityWellSpawn : MonoBehaviour
+{
+    public GameObject GravityWellPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, 
+                Input.mousePosition.y, 0f);
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            worldPos.y = 0;
+            Instantiate(GravityWellPrefab, worldPos, Quaternion.identity);
+        }        
+    }
+}
